4dd3aecacacf25684946101212543d3843d5167c
Kaare Hansen <kaare@gshansen.dk>
2023-11-12 14:48:14 +0100

    Tests

--- Assignments/A3/tests/test_geomUtils.py
+++ Assignments/A3/tests/test_geomUtils.py
@@ -6,16 +6,19 @@ Kaare G. S. Hansen, s214282 - DTU
 import pytest
 
 from PyConBIM import geomUtils
-from OCC.Core.gp import gp_Pnt
+# from OCC.Core.gp import gp_Pnt
 
-def test_distance_between_points():
-    p1 = gp_Pnt(0.0, 0.0, 0.0)
-    p2 = gp_Pnt(1.0, 1.0, 1.0)
+# def test_distance_between_points():
+#     p1 = gp_Pnt(0.0, 0.0, 0.0)
+#     p2 = gp_Pnt(1.0, 1.0, 1.0)
 
-    distance = geomUtils.distance_between_points(p1, p2)
+#     distance = geomUtils.distance_between_points(p1, p2)
 
-    expected_distance = 1.7320508075688772
-    assert abs(distance - expected_distance) < 0.001
+#     expected_distance = 1.7320508075688772
+#     assert abs(distance - expected_distance) < 0.001
 
-    wrong_distance = 1.8320508075688772
-    assert not abs(distance - wrong_distance) < 0.001
+#     wrong_distance = 1.8320508075688772
+#     assert not abs(distance - wrong_distance) < 0.001
+
+def test_sampleUtil():
+    assert geomUtils.exampleFunc(1,2) == 3
